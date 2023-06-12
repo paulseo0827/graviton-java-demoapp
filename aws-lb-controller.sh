6d1b9e02f4719635a085805be6b9fa0cb44ed1ab
EC2 Default User <ec2-user@ip-172-31-31-126.ap-northeast-2.compute.internal>
2023-06-12 18:31:57 +0000

    commit

--- aws-lb-controller.sh
+++ aws-lb-controller.sh
@@ -1,6 +1,6 @@
 export LBC_VERSION="v2.5.2"
 export LBC_CHART_VERSION="1.5.3"
-export ACCOUNT_ID="837641087863"
+export ACCOUNT_ID="REPLACE_ACCOUNT_ID"
 export EKS_CLUSTER="graviton-workshop"
 
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
