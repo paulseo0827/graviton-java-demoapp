fe3b9b83f7e1c0d2b7aad88ceaf0e4964becf0d0
paulseo0827 <miyus1004@empal.com>
2023-06-16 09:54:58 +0900

    Update change_account_id.sh

--- change_account_id.sh
+++ change_account_id.sh
@@ -4,4 +4,6 @@ account_id=$1
 sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" makefile
 sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" deploy/kubernetes/deployment-arm64.yaml
 sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" deploy/kubernetes/deployment-x86.yaml
+sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" deploy/kubernetes/deployment-arm64-multiarchmanifest.yaml
+sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" deploy/kubernetes/deployment-x86-multiarchmanifest.yaml
 sed -i "s/REPLACE_ACCOUNT_ID/$account_id/g" aws-lb-controller.sh
